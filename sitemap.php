a37c0602161d58bfd6979fd4f2a80355b3855aad
Petr Hučík <petr@getdatakick.com>
2024-05-17 09:57:28 +0200

    Release sitemap v4.1.3

--- sitemap.php
+++ sitemap.php
@@ -50,7 +50,7 @@ class Sitemap extends Module
     {
         $this->name = 'sitemap';
         $this->tab = 'seo';
-        $this->version = '4.1.2';
+        $this->version = '4.1.3';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
